d80d4cd3bbd6a7834016a0bfaa815d5251b77ea6
Seno Dwi Prasetyo <senothoha77@gmail.com>
2024-09-27 05:04:40 +0700

    add belongtobolt

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use LaraZeus\Bolt\Models\Concerns\BelongToBolt;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, BelongToBolt;
 
     /**
      * The attributes that are mass assignable.
